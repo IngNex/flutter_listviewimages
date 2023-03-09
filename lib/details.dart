d2d2ab111077135e274d4f1b63f29963896bb9ef
IngNex <ingnexmaicol14@gmail.com>
2023-03-09 12:42:55 -0500

    page details Citys

---
+++ lib/details.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DetailsPages extends StatelessWidget {
+  //const DetailsPages({Key? key, required this.city}): super(key:key);
+
+  //final City city;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+//https://youtu.be/J5GYpNCQjsI?t=1035
